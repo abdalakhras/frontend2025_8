afc3ace231643f06706f70d4562031e6f24bbde5
abdalakhras <abdakalakhras@gmail.com>
2025-02-22 11:47:32 +0300

    two css files added (light and dark ) and called by js

--- Js/grid2.js
+++ Js/grid2.js
@@ -4,25 +4,32 @@ var nav = document.getElementById('nav')
     nav.style.backgroundColor='pink'
 
 function changeTheme(color){
-    console.log(color);
-    document.body.style.backgroundColor = color
+    ///console.log(color);
+    //document.body.style.backgroundColor = color
 
 
-    let elemnts= document.querySelectorAll('#nav,#leftMenu,#main,#rightMenu,#footer')
-console.log(elemnts)
+    //let elemnts= document.querySelectorAll('#nav,#leftMenu,#main,#rightMenu,#footer')
+//console.log(elemnts)
+var theme= document.getElementById('theme')
 if(color=='black'){
-    for (let i = 0; i < elemnts.length;i++){
+   
+    console.log(theme)
+    theme.href='CSS/grid2dark.css'
+    /*for (let i = 0; i < elemnts.length;i++){
         elemnts[i].style.backgroundColor = 'black'
         elemnts[i].style.border= '2px white solid'
         elemnts[i].style.color='white'
-    }
+    }*/
 }
 else{
-    for (let i = 0; i < 4;i++){
+
+    theme.href='CSS/grid2light.css'
+    console.log(theme)
+   /* for (let i = 0; i < 4;i++){
         elemnts[i].style.backgroundColor = 'aqua'
         elemnts[i].style.border= '2px black solid'
         elemnts[i].style.color='black'
-    }
+    }*/
     nav.style.backgroundColor='red'
 }
 
