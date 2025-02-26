227a77605cf9dccb17b23b4046f3d0cdcdd2ab16
abdalakhras <abdakalakhras@gmail.com>
2025-02-26 11:53:10 +0300

    Update progress.js

--- Js/progress.js
+++ Js/progress.js
@@ -10,12 +10,14 @@ function prog(){
 
     if(width==100){
         clearInterval(x);
+        console.log('finish')
 
     }
     else{
         width++;
         elem.style.width= width + '%';
-        
+
+        console.log('progress')
         // elem.style.backgroundColor = 'red'
 
     }
