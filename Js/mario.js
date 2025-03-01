d93464f0d0032df2720a79c7832725d2520f791a
abdalakhras <abdakalakhras@gmail.com>
2025-03-01 21:58:39 +0300

    changing the position of mario character

--- Js/mario.js
+++ Js/mario.js
@@ -1,21 +1,39 @@
 
 function move(direction){
     var mario = document.getElementById('mario')
+    var positionLeft = parseInt(window.getComputedStyle(mario).left, 10);
+    var positionTop = parseInt(window.getComputedStyle(mario).top, 10);
+    //let position = mario.style.left
 
     if(direction == 'up'){
-        console.log(direction)
-       
 
+        mario.style.top = (positionTop - 10) + 'px'
+        console.log(positionTop)
+        console.log(direction)
     }
     if(direction == 'down'){
-        //console.log(direction)
+        mario.style.top = (positionTop + 10) + 'px'
+        console.log(positionTop)
+        console.log(direction)
     }
+    
+
+    
     if(direction=='right'){
-        //console.log(direction)
+       
+        console.log(direction)
+        mario.style.left = (positionLeft + 10) + 'px'
+        console.log(positionLeft)
+
 
     }
-    if(direction=='left'){
-        //console.log(direction)
+    if(direction == 'left'){
+        mario.style.left = (positionLeft - 10) + 'px'
+        console.log(positionLeft)
+        console.log(direction)
+        
+        
+
 
     }
 }
