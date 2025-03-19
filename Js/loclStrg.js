170847d160e50435ec125155a326475b0d147b66
abdalakhras <abdakalakhras@gmail.com>
2025-03-19 23:34:22 +0300

    adding local storage to an image
    adding local storage to an image , while changing the image we store a new value of another image , and the image stays after refreshing the page , unless you clicked again to change the image

--- Js/loclStrg.js
+++ Js/loclStrg.js
@@ -7,7 +7,8 @@ var body = document.getElementById('body')
 var btn = document.getElementById("btn0")
 
 
-var themeColor = localStorage.getItem('theme') 
+var themeColor = localStorage.getItem('theme') || 'white';
+// localStorage.setItem('theme',themeColor) // Ensure themeColor is stored in local storage, this is optional and it works well without it 
     console.log(themeColor) // the local storage is white and i don't know why
  body.style.backgroundColor = themeColor // this is to make sure that the backgroundColor is not changed after refresh 
  btn.innerHTML = themeColor === 'white' ? 'dark mode' : 'light mode'; //this is to make sure that innertext of the button is not changed after refresh 
@@ -21,12 +22,13 @@ if(localStorage.getItem('theme') === 'white'){
     localStorage.setItem('theme','black')
  body.style.backgroundColor= 'black'
  btn.innerHTML = 'light mode'
- 
+ age1.style.color= 'red'
 }
 else{
     localStorage.setItem('theme','white')
 body.style.backgroundColor= 'white'
  btn.innerHTML ='dark mode'
+ age1.style.color= 'green'
 }
 }
 
@@ -47,14 +49,24 @@ body.style.backgroundColor= 'white'
 //     }
 // }
 
+var changepicture = localStorage.getItem('img') 
+console.log(changepicture)
+ image.src = changepicture // this is to make sure that the image is not changed after refresh
+ imgbtn.innerHTML = changepicture === 'images/DOG.png' ? 'cat' : 'dog' //this is to make sure that innertext of the button is not changed after refresh 
+
 
 function changeImg(position){
+    var imgbtn = document.getElementById('imgbtn')
     var image = document.getElementById('image')
-if(position == 'left')
-image.src = "/images/cat.png"
-
+if(localStorage.getItem('img') == '/images/cat.png'){
+  localStorage.setItem('img','images/DOG.png')
+image.src = "/images/DOG.png"
+imgbtn.innerHTML = 'cat'
+}
 else{
-    image.src = "images/DOG.png"
+    localStorage.setItem('img','/images/cat.png')
+    image.src = "images/cat.png"
+    imgbtn.innerHTML = 'dog'
 }
 }
 
@@ -79,4 +91,5 @@ console.log(student.age)
  body.appendChild(age1)
  body.appendChild(name1)
  
+
 
